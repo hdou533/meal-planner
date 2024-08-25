aa18c821b2985fdb154f234ed8e3fbc7fb6d16e9
Sally <sallydou2023@gmail.com>
2024-08-25 23:51:54 +1200

    ui fix

--- app/my-meal-plans/page.tsx
+++ app/my-meal-plans/page.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 const MyMealPlansPage = () => {
   return (
-    <main className="flex flex-col items-center justify-center gap-5 p-4">
+    <main className="flex flex-col items-center justify-center gap-5 p-24">
       <h2 className="text-2xl">My Meal Plans</h2>
       <Link href="/meal-planner">
         <Button>Create a Plan</Button>
--- components/ui/footer/Footer.tsx
+++ components/ui/footer/Footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const year = new Date().getFullYear();
 const Footer = () => {
   return (
-    <footer className="bottom-5 w-full text-xs ">
+    <footer className="bottom-5 left-5 text-xs fixed">
       <p className="text-center">© {year}</p>
     </footer>
   );
